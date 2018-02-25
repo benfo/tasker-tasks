da8410b362ba66b64939046de6d8e95b60c95b76
Ben Fourie <ben.fourie@gmail.com>
2018-02-25 08:32:22 +0200

    Add sinon mock

--- test/test.js
+++ test/test.js
@@ -1,3 +1,26 @@
 import test from 'ava'
+import sinon from 'sinon'
+import * as Tasker from '../sdk/tasker'
+import * as HtmlTools from '../src/tools/html'
+import Tasks from '../src/tasks'
 
-test.todo('write some tests')
+let sandbox
+
+test.beforeEach(t => {
+  sandbox = sinon.sandbox.create()
+})
+
+test.afterEach.always(() => {
+  sandbox.restore()
+})
+
+test('write some tests', t => {
+  // sandbox
+  //   .stub(HtmlTools, 'queryHtml')
+  //   .returns(Promise.resolve('bar'))
+  // sandbox.stub(Tasker, 'setLocal')
+
+  Tasks.packtpub.run()
+
+  t.pass()
+})
